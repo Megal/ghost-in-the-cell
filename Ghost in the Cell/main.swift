95713fe1ac85fc87b2eddbc3792b92abc77fde07
Svyatoshenko "Megal" Misha <m1sha.odesk@gmail.com>
2017-02-25 21:19:38 +0300

    Initial Commit

---
+++ Ghost in the Cell/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ghost in the Cell
+//
+//  Created by Svyatoshenko "Megal" Misha on 2017-02-25.
+//  Copyright © 2017 Megal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
